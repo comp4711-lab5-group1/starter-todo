bd971b547e690b7290b147dfd337539067afeef0
Steven Tsao <steven.tsao777@gmail.com>
2017-03-08 15:28:06 -0800

    added new item and task buttons for the owner

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -32,7 +32,13 @@
             {
                 if (!empty($task->status))
                     $task->status = $this->statuses->get($task->status)->name;
-                $result .= $this->parser->parse('oneitem', (array) $task, true);
+                //$result .= $this->parser->parse('oneitem', (array) $task, true);
+                
+                // INSERT the next three lines. The fourth is already there
+                if ($role == ROLE_OWNER)
+                    $result .= $this->parser->parse('oneitemx', (array) $task, true);
+                else
+                    $result .= $this->parser->parse('oneitem', (array) $task, true);
             }
             $this->data['display_tasks'] = $result;
 
@@ -59,6 +65,12 @@
                 if ($count >= $this->items_per_page) break;
             }
             $this->data['pagination'] = $this->pagenav($num);
+            //$this->show_page($tasks);
+            
+            // INSERT next three lines
+            $role = $this->session->userdata('userrole');
+            if ($role == ROLE_OWNER) 
+                    $this->data['pagination'] .= $this->parser->parse('itemadd',[], true);
             $this->show_page($tasks);
         }
          
